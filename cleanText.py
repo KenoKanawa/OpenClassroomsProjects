b25e9eed1e01d4eb808d0b97f6daced11c7c30ab
KenoKanawa <cyril.calmettes@icloud.com>
2022-04-06 15:53:48 +0200

    Create cleanText.py

--- cleanText.py
+++ cleanText.py
@@ -3,6 +3,7 @@ import re
 import html as ihtml
 import nltk
 nltk.download('words')
+nltk.download('wordnet')
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
 nltk.download('omw-1.4')
